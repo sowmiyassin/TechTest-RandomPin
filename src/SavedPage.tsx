80129b1b370b4604d2d48fd404e58dd9c4ac3db7
sowmiya <sowmiya.s02@infosys.com>
2020-01-13 01:12:29 +0530

    Saved Pins after generation
    Saved Pins after generation

--- src/SavedPage.tsx
+++ src/SavedPage.tsx
@@ -2,18 +2,48 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Table } from 'react-bootstrap';
 import './App.css';
-
-export class SavedPage extends Component<{}> {
+import { saveAction } from './saveAction';
+type savedPinsState = {
+    savedPins: any
+  }
+export class SavedPage extends Component<{}, savedPinsState> {
     constructor(props: any) {
         super(props);
         console.log(JSON.stringify(props));
-
+        this.state = {
+            savedPins: []
+        }
+        this.renderPins = this.renderPins.bind(this);
     }
-
-    render() {
+    UNSAFE_componentWillReceiveProps(nextProps: any) {
+        this.setState({savedPins: nextProps.saveReducer.storedPins});
+    }
+    renderPins(rowPin: any, index: any) {
         return (
-  
-    <div></div>
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{`Pin ${index + 1}`}</td>
+            <td>{rowPin.pin}</td>
+          </tr>
+        )
+      }
+      render() {
+        return (
+            <div className="App">
+                <h2>Saved Pins</h2>
+                <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Name</th>
+      <th>Pin</th>
+    </tr>
+  </thead>
+  <tbody>
+    {this.state.savedPins.map(this.renderPins)}
+  </tbody>
+</Table>
+    </div>
         )
     }
 }
@@ -22,6 +52,6 @@ const mapStateToProps = (state: {}) => ({
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
-  
+    saveAction: (payload: any) => dispatch(saveAction(payload))
 })
 export default connect(mapStateToProps, mapDispatchToProps)(SavedPage);
