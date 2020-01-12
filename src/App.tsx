e5eea3d892900ebab72b7df51de44dd2ef122da3
sowmiya <sowmiya.s02@infosys.com>
2020-01-12 23:28:25 +0530

    page route added
    page route added

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,18 @@ import {
 import { Tabs, Tab, Nav } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import './App.css';
+import GeneratePage from './GeneratePage';
+import SavedPage from './SavedPage';
 const App: React.FC = () => {
   return (
     <Router>
       
       <Tabs defaultActiveKey="generate" id="uncontrolled-tab-example" variant="pills">
         <Tab eventKey="generate" title="Generate">
-         
+        <GeneratePage />
         </Tab>
         <Tab eventKey="saved" title="Saved">
-        
+        <SavedPage />
         </Tab>
       </Tabs>
     </Router>
