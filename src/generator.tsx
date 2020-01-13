9225b405def2085feb754ad748392b243d7b3138
sowmiyassin <sowmiya.ss.in@gmail.com>
2020-01-13 04:19:38 -0800

    added validation for consecutive ,assending and desending validation through regex
    added validation for consecutive ,assending and desending validation through regex

--- src/generator.tsx
+++ src/generator.tsx
@@ -1,14 +1,29 @@
 
 
+function validatePin(pinValue: any) {
+  if(pinValue != "" && pinValue != null && pinValue != undefined) {
+    if (/(00|11|22|33|44|55|66|77|88|99|012|123|234|345|456|567|678|789|890|210|321|432|543|654|765|876|987|098)/.test(pinValue)) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
 function fourDigitRandomizer(): any {
   var fourDigitPin = Math.floor(1000 + Math.random() * 9000);
-
-  return fourDigitPin;
+  if(validatePin(fourDigitPin) == true) {
+    return fourDigitRandomizer();
+  } else {
+    return fourDigitPin;
+  }
 }
 
 export default function () {
     var firstPin = fourDigitRandomizer();
-    var secondPin =fourDigitRandomizer();
+    var secondPin = fourDigitRandomizer();
     var thirdPin = fourDigitRandomizer();
     var fourthPin = fourDigitRandomizer();
     var fifthPin = fourDigitRandomizer();
