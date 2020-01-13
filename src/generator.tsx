90695fe04713dc241c6068fe8734e66c7bdb11d3
sowmiyassin <sowmiya.ss.in@gmail.com>
2020-01-13 01:07:37 -0800

    Pin generation logic fix
    Pin generation logic fix

--- src/generator.tsx
+++ src/generator.tsx
@@ -2,7 +2,7 @@ export default function () {
     let counts: any = {};
     let validCounts: any = {};
     let randomNumber;
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 1; i++) {
         let firstDigit = Math.floor(Math.random() * 10);
         let secondDigit = Math.floor(Math.random() * 10);
         let thirdDigit = Math.floor(Math.random() * 10);
@@ -14,20 +14,35 @@ export default function () {
             thirdDigit,
             fourthDigit
         };
-        validCounts[i] = {
-            validDigitOne,
-            validDigitTwo,
-            validDigitThree,
-            validDigitFour
-        };
-        //** start validation on 2 consecutive digits********// 
-        validCounts[i].validDigitOne = (counts[i].firstDigit !== counts[i].secondDigit);
-        validCounts[i].validDigitTwo = ((counts[i].secondDigit !== counts[i].thirdDigit) && (counts[i].secondDigit !== counts[i].firstDigit));
-        validCounts[i].validDigitThree = ((counts[i].thirdDigit !== counts[i].fourthDigit) && (counts[i].thirdDigit !== counts[i].secondDigit));
-        validCounts[i].validDigitFour = ((counts[i].fourthDigit !== counts[i].thirdDigit));
-        //** end validation on 2 consecutive digits********// 
+        // validCounts[i] = {
+        //     validDigitOne,
+        //     validDigitTwo,
+        //     validDigitThree,
+        //     validDigitFour
+        // };
+        // //** start validation on 2 consecutive digits********// 
+        // validCounts[i].validDigitOne = (counts[i].firstDigit !== counts[i].secondDigit);
+        // validCounts[i].validDigitTwo = ((counts[i].secondDigit !== counts[i].thirdDigit) && (counts[i].secondDigit !== counts[i].firstDigit));
+        // validCounts[i].validDigitThree = ((counts[i].thirdDigit !== counts[i].fourthDigit) && (counts[i].thirdDigit !== counts[i].secondDigit));
+        // validCounts[i].validDigitFour = ((counts[i].fourthDigit !== counts[i].thirdDigit));
+        // //** end validation on 2 consecutive digits********// 
 
-        let validRandomNumber = ((validCounts[i].validDigitOne && validCounts[i].validDigitTwo && validCounts[i].validDigitThree && validCounts[i].validDigitFour));
+        //
+var pin=firstDigit+""+secondDigit+""+thirdDigit+""+fourthDigit;
+console.log("pin "+pin);
+let validatePIN = function(pin : any) {
+  if (/^(^(?!.*(11|22|33|44|55|66|77|88|99|0123|1234|2345|3456|4567|5678|6789|7890|4321|5432|6543|7654|8765|9876|0987))\d{4}$)$/.test(pin)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//
+
+
+        let validRandomNumber = validatePIN(pin);
+     //   ((validCounts[i].validDigitOne && validCounts[i].validDigitTwo && validCounts[i].validDigitThree && validCounts[i].validDigitFour));
         if (validRandomNumber === false) {
             let firstDigit = Math.floor(Math.random() * 10);
             let secondDigit = Math.floor(Math.random() * 10);
