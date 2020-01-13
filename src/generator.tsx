232c46b044e10dce1761df2624e2c86c265f724d
sowmiyassin <sowmiya.ss.in@gmail.com>
2020-01-13 04:30:19 -0800

    validation on pin number should not match another generated pin
    validation on pin number should not match another generated pin

--- src/generator.tsx
+++ src/generator.tsx
@@ -1,4 +1,52 @@
-
+function getNewIfMatched(pinA: any, pinB: any, pinC: any, pinD: any, pinE: any, stepCount: any): any {
+  if(stepCount == 2) {
+      if(pinB == pinA) {
+          pinB = fourDigitRandomizer();
+          if(pinB == pinA) {
+              return getNewIfMatched(pinA, pinB, null, null, null, 2);
+          } else {
+              return pinB;
+          }
+      } else {
+          return pinB;
+      }
+  } else if(stepCount == 3) {
+      if(pinC == pinA || pinC == pinB) {
+          pinC = fourDigitRandomizer();
+          if(pinC == pinA || pinC == pinB) {
+              return getNewIfMatched(pinA, pinB, pinC, null, null, 3);
+          } else {
+              return pinC;
+          }
+      } else {
+          return pinC;
+      }
+  } else if(stepCount == 4) {
+      if(pinD == pinA || pinD == pinB || pinD == pinC) {
+          pinD = fourDigitRandomizer();
+          if(pinD == pinA || pinD == pinB || pinD == pinC) {
+              return getNewIfMatched(pinA, pinB, pinC, pinD, null, 4);
+          } else {
+              return pinD;
+          }
+      } else {
+          return pinD;
+      }
+  } else if(stepCount == 5) {
+      if(pinE == pinA || pinE == pinB || pinE == pinC || pinE == pinD) {
+          pinE = fourDigitRandomizer();
+          if(pinE == pinA || pinE == pinB || pinE == pinC || pinE == pinD) {
+              return getNewIfMatched(pinA, pinB, pinC, pinD, pinE, 5);
+          } else {
+              return pinE;
+          }
+      } else {
+          return pinE;
+      }
+  } else {
+      return false;
+  }
+}
 
 function validatePin(pinValue: any) {
   if(pinValue != "" && pinValue != null && pinValue != undefined) {
@@ -23,10 +71,10 @@ function fourDigitRandomizer(): any {
 
 export default function () {
     var firstPin = fourDigitRandomizer();
-    var secondPin = fourDigitRandomizer();
-    var thirdPin = fourDigitRandomizer();
-    var fourthPin = fourDigitRandomizer();
-    var fifthPin = fourDigitRandomizer();
+    var secondPin = getNewIfMatched(firstPin, fourDigitRandomizer(), null, null, null, 2);
+    var thirdPin = getNewIfMatched(firstPin, secondPin, fourDigitRandomizer(), null, null, 3);
+    var fourthPin = getNewIfMatched(firstPin, secondPin, thirdPin, fourDigitRandomizer(), null, 4);
+    var fifthPin = getNewIfMatched(firstPin, secondPin, thirdPin, fourthPin, fourDigitRandomizer(), 5);
 
   return firstPin + " - " + secondPin + " - " + thirdPin + " - " + fourthPin + " - " + fifthPin;
 }
